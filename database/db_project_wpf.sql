d9531d0fe330fe7a510e9ba3e782f19ebc4f38c6
RoyEvan <77206268+RoyEvan@users.noreply.github.com>
2024-05-10 20:55:16 +0700

    Update db_project_wpf.sql

--- database/db_project_wpf.sql
+++ database/db_project_wpf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 03:51 PM
+-- Generation Time: May 10, 2024 at 03:55 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -62,6 +62,20 @@ CREATE TABLE `courses` (
   `COURSE_LENGTH` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`COURSE_ID`, `TEACHER_ID`, `COURSE_NAME`, `COURSE_DESC`, `COURSE_LEVEL`, `COURSE_CLASS`, `COURSE_DAY`, `COURSE_LENGTH`) VALUES
+('C0001', 'T2024001', 'English', 'You will learn basic English for daily life!', 1, 'A-201', 'Monday', 1.5),
+('C0002', 'T2024001', 'English', 'You will learn complex English for complex conversations!', 2, 'A-202', 'Thursday', 2.5),
+('C0003', 'T2024002', 'Mandarin', 'You will learn basic Mandarin for daily life!', 1, 'A-203', 'Monday', 1.5),
+('C0004', 'T2024002', 'Mandarin', 'You will learn complex Mandarin for complex conversations!', 2, 'A-204', 'Tuesday', 2.5),
+('C0005', 'T2024003', 'Spanish', 'You will learn basic Spanish for daily life!', 1, 'A-205', 'Saturday', 1.5),
+('C0006', 'T2024003', 'Spanish', 'You will learn complex Spanish for complex conversations!', 2, 'A-206', 'Thursday', 2.5),
+('C0007', 'T2024004', 'Japanese', 'You will learn basic Japanese for daily life!', 1, 'A-207', 'Saturday', 1.5),
+('C0008', 'T2024004', 'Japanese', 'You will learn complex Japanese for complex conversations!', 2, 'A-208', 'Friday', 2.5);
+
 -- --------------------------------------------------------
 
 --
@@ -70,7 +84,8 @@ CREATE TABLE `courses` (
 
 CREATE TABLE `courses_taken` (
   `STUDENT_ID` varchar(8) NOT NULL,
-  `COURSE_ID` varchar(5) NOT NULL
+  `COURSE_ID` varchar(5) NOT NULL,
+  `IS_FINISHED` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -113,6 +128,16 @@ CREATE TABLE `students` (
   `STUDENT_PASSWORD` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`STUDENT_ID`, `STUDENT_USERNAME`, `STUDENT_NAME`, `STUDENT_EMAIL`, `STUDENT_ADDRESS`, `STUDENT_PHONE`, `STUDENT_PASSWORD`) VALUES
+('S2400001', 'AlbertV24', 'Albert@456', 'Albert Valentino Utomo', 'albert.v22@mhs.istts.ac.id', 'Jalan Yang Pe', '81322310662'),
+('S2400002', 'KevinJo24', 'Kevin@234', 'Kevin Jonathan', 'kevin.j22@mhs.istts.ac.id', 'Jalan Kenanga', '81322310664'),
+('S2400003', 'Raymond24', 'Raymond@345', 'Raymond Lyanto Hoentoro', 'raymond.l22@mhs.istts.ac.id', 'Jalan Tidak B', '81322310667'),
+('S2400004', 'RoyEvan24', 'Roy@123', 'Roy Evan Wiguna', 'roy.e22@mhs.istts.ac.id', 'Jalan Ku Berp', '81322310670');
+
 -- --------------------------------------------------------
 
 --
@@ -128,6 +153,16 @@ CREATE TABLE `teachers` (
   `TEACHER_PHONE` varchar(13) NOT NULL,
   `TEACHER_PASSWORD` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`TEACHER_ID`, `TEACHER_USERNAME`, `TEACHER_NAME`, `TEACHER_EMAIL`, `TEACHER_ADDRESS`, `TEACHER_PHONE`, `TEACHER_PASSWORD`) VALUES
+('T2024001', 'Albert', 'Albert@456', 'Albert Valentino', 'albert.v22@teach.istts.ac.id', 'Jalan Yang Pe', '81322310662'),
+('T2024002', 'Kevin', 'Kevin@234', 'Kevin Jonathan', 'kevin.j22@teach.istts.ac.id', 'Jalan Kenanga', '81322310664'),
+('T2024003', 'Raymond', 'Raymond@345', 'Raymond Lyanto', 'raymond.l22@teach.istts.ac.id', 'Jalan Tidak B', '81322310667'),
+('T2024004', 'Roy', 'Roy@123', 'Roy Evan', 'roy.e22@teach.istts.ac.id', 'Jalan Ku Berp', '81322310670');
 
 --
 -- Indexes for dumped tables
@@ -246,116 +281,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-INSERT INTO students VALUES("S2400001", "AlbertV24", "Albert@456", "Albert Valentino Utomo", "albert.v22@mhs.istts.ac.id", "Jalan Yang Penuh Kenangan No. 6-10 Blok A", "81322310662");
-INSERT INTO students VALUES("S2400002", "KevinJo24", "Kevin@234", "Kevin Jonathan", "kevin.j22@mhs.istts.ac.id", "Jalan Kenangan No. 45-47", "81322310664");
-INSERT INTO students VALUES("S2400003", "Raymond24", "Raymond@345", "Raymond Lyanto Hoentoro", "raymond.l22@mhs.istts.ac.id", "Jalan Tidak Buntu No. 73-79", "81322310667");
-INSERT INTO students VALUES("S2400004", "RoyEvan24", "Roy@123", "Roy Evan Wiguna", "roy.e22@mhs.istts.ac.id", "Jalan Ku Berpisah Dengan Mu No. 73-77", "81322310670");
-
-INSERT INTO teachers VALUES("T2024001", "Albert", "Albert@456", "Albert Valentino", "albert.v22@teach.istts.ac.id", "Jalan Yang Penuh Kenangan No. 6-10 Blok A", "81322310662");
-INSERT INTO teachers VALUES("T2024002", "Kevin", "Kevin@234", "Kevin Jonathan", "kevin.j22@teach.istts.ac.id", "Jalan Kenangan No. 45-47", "81322310664");
-INSERT INTO teachers VALUES("T2024003", "Raymond", "Raymond@345", "Raymond Lyanto", "raymond.l22@teach.istts.ac.id", "Jalan Tidak Buntu No. 73-79", "81322310667");
-INSERT INTO teachers VALUES("T2024004", "Roy", "Roy@123", "Roy Evan", "roy.e22@teach.istts.ac.id", "Jalan Ku Berpisah Dengan Mu No. 73-77", "81322310670");
-
-INSERT INTO courses VALUES("C0001", "T2024001", "English", "You will learn basic English for daily life!", 1, "A-201", "Monday", 1.5);
-INSERT INTO courses VALUES("C0002", "T2024001", "English", "You will learn complex English for complex conversations!", 2, "A-202", "Thursday", 2.5);
-INSERT INTO courses VALUES("C0003", "T2024002", "Mandarin", "You will learn basic Mandarin for daily life!", 1, "A-203", "Monday", 1.5);
-INSERT INTO courses VALUES("C0004", "T2024002", "Mandarin", "You will learn complex Mandarin for complex conversations!", 2, "A-204", "Tuesday", 2.5);
-INSERT INTO courses VALUES("C0005", "T2024003", "Spanish", "You will learn basic Spanish for daily life!", 1, "A-205", "Saturday", 1.5);
-INSERT INTO courses VALUES("C0006", "T2024003", "Spanish", "You will learn complex Spanish for complex conversations!", 2, "A-206", "Thursday", 2.5);
-INSERT INTO courses VALUES("C0007", "T2024004", "Japanese", "You will learn basic Japanese for daily life!", 1, "A-207", "Saturday", 1.5);
-INSERT INTO courses VALUES("C0008", "T2024004", "Japanese", "You will learn complex Japanese for complex conversations!", 2, "A-208", "Friday", 2.5);
-
-INSERT INTO courses_taken VALUES("S2400001", "C0005", 1);
-INSERT INTO courses_taken VALUES("S2400002", "C0001", 1);
-INSERT INTO courses_taken VALUES("S2400001", "C0001", 0);
-INSERT INTO courses_taken VALUES("S2400001", "C0006", 0);
-INSERT INTO courses_taken VALUES("S2400001", "C0007", 0);
-INSERT INTO courses_taken VALUES("S2400002", "C0003", 1);
-INSERT INTO courses_taken VALUES("S2400002", "C0002", 0);
-INSERT INTO courses_taken VALUES("S2400002", "C0004", 0);
-INSERT INTO courses_taken VALUES("S2400003", "C0001", 0);
-INSERT INTO courses_taken VALUES("S2400003", "C0005", 1);
-INSERT INTO courses_taken VALUES("S2400003", "C0003", 0);
-INSERT INTO courses_taken VALUES("S2400003", "C0007", 1);
-INSERT INTO courses_taken VALUES("S2400004", "C0001", 1);
-INSERT INTO courses_taken VALUES("S2400003", "C0006", 0);
-INSERT INTO courses_taken VALUES("S2400003", "C0008", 0);
-INSERT INTO courses_taken VALUES("S2400004", "C0002", 0);
-INSERT INTO courses_taken VALUES("S2400004", "C0003", 0);
-INSERT INTO courses_taken VALUES("S2400004", "C0005", 0);
-
-INSERT INTO materials VALUES(0, "C0001", "Week 0", "Introduction to English!");
-INSERT INTO materials VALUES(0, "C0003", "Week 0", "Introduction to Mandarin!");
-INSERT INTO materials VALUES(0, "C0004", "Week 0", "Introduction to Complex Mandarin!");
-INSERT INTO materials VALUES(0, "C0002", "Week 0", "Introduction to Complex English!");
-INSERT INTO materials VALUES(0, "C0006", "Week 0", "Introduction to Complex Spanish!");
-INSERT INTO materials VALUES(0, "C0008", "Week 0", "Introduction to Complex Japanese!");
-INSERT INTO materials VALUES(0, "C0005", "Week 0", "Introduction to Spanish");
-INSERT INTO materials VALUES(0, "C0007", "Week 0", "Introduction to Japanese!");
-
-INSERT INTO materials VALUES(0, "C0001", "Week 1", "Basic English Sentences and Questions");
-INSERT INTO materials VALUES(0, "C0003", "Week 1", "Basic Mandarin Sentences and Questions");
-INSERT INTO materials VALUES(0, "C0004", "Week 1", "Talk about our holidays in Mandarin!");
-INSERT INTO materials VALUES(0, "C0002", "Week 1", "Talk about our holidays in English!");
-INSERT INTO materials VALUES(0, "C0006", "Week 1", "Talk about our holidays in Spanish!");
-INSERT INTO materials VALUES(0, "C0008", "Week 1", "Talk about our holidays in Japanese!");
-INSERT INTO materials VALUES(0, "C0005", "Week 1", "Basic Spanish Sentences and Questions");
-INSERT INTO materials VALUES(0, "C0007", "Week 1", "Basic Japanese Sentences and Questions");
-
-INSERT INTO materials VALUES(0, "C0001", "Week 2", "Basic Tenses in English!");
-INSERT INTO materials VALUES(0, "C0003", "Week 2", "Speaking Basic Sentences in Mandarin!");
-INSERT INTO materials VALUES(0, "C0004", "Week 2", "Making Poetry in Mandarin!");
-INSERT INTO materials VALUES(0, "C0002", "Week 2", "Making Poetry in English!");
-INSERT INTO materials VALUES(0, "C0006", "Week 2", "Making Poetry in Spanish!");
-INSERT INTO materials VALUES(0, "C0008", "Week 2", "Making Poetry in Japanese!");
-INSERT INTO materials VALUES(0, "C0005", "Week 2", "Speaking Basic Sentences in Spanish!");
-INSERT INTO materials VALUES(0, "C0007", "Week 2", "Speaking Basic Sentences in Japanese!");
-
-INSERT INTO materials VALUES(0, "C0001", "Week 3", "Talk about our family and friends in English!");
-INSERT INTO materials VALUES(0, "C0003", "Week 3", "Writing basic sentences and a story about family in Mandarin!");
-INSERT INTO materials VALUES(0, "C0004", "Week 3", "Guess these chinese songs lyrics!");
-INSERT INTO materials VALUES(0, "C0002", "Week 3", "Guess what is the meaniong of these English songs!");
-INSERT INTO materials VALUES(0, "C0006", "Week 3", "Guess what is the meaniong of these Spanish songs!");
-INSERT INTO materials VALUES(0, "C0008", "Week 3", "Guess what is the meaniong of these Japanese songs!");
-INSERT INTO materials VALUES(0, "C0005", "Week 3", "Talk about our family and friends in Spanish!");
-INSERT INTO materials VALUES(0, "C0007", "Week 3", "Talk about our family and friends in Japanese!");
-
-INSERT INTO assignments VALUES(0, "C0001", "Week 0");
-INSERT INTO assignments VALUES(0, "C0003", "Week 0");
-INSERT INTO assignments VALUES(0, "C0004", "Week 0");
-INSERT INTO assignments VALUES(0, "C0002", "Week 0");
-INSERT INTO assignments VALUES(0, "C0006", "Week 0");
-INSERT INTO assignments VALUES(0, "C0008", "Week 0");
-INSERT INTO assignments VALUES(0, "C0005", "Week 0");
-INSERT INTO assignments VALUES(0, "C0007", "Week 0");
-
-INSERT INTO assignments VALUES(0, "C0001", "Week 1");
-INSERT INTO assignments VALUES(0, "C0003", "Week 1");
-INSERT INTO assignments VALUES(0, "C0004", "Week 1");
-INSERT INTO assignments VALUES(0, "C0002", "Week 1");
-INSERT INTO assignments VALUES(0, "C0006", "Week 1");
-INSERT INTO assignments VALUES(0, "C0008", "Week 1");
-INSERT INTO assignments VALUES(0, "C0005", "Week 1");
-INSERT INTO assignments VALUES(0, "C0007", "Week 1");
-
-INSERT INTO assignments VALUES(0, "C0001", "Week 2");
-INSERT INTO assignments VALUES(0, "C0003", "Week 2");
-INSERT INTO assignments VALUES(0, "C0004", "Week 2");
-INSERT INTO assignments VALUES(0, "C0002", "Week 2");
-INSERT INTO assignments VALUES(0, "C0006", "Week 2");
-INSERT INTO assignments VALUES(0, "C0008", "Week 2");
-INSERT INTO assignments VALUES(0, "C0005", "Week 2");
-INSERT INTO assignments VALUES(0, "C0007", "Week 2");
-
-INSERT INTO assignments VALUES(0, "C0001", "Week 3");
-INSERT INTO assignments VALUES(0, "C0003", "Week 3");
-INSERT INTO assignments VALUES(0, "C0004", "Week 3");
-INSERT INTO assignments VALUES(0, "C0002", "Week 3");
-INSERT INTO assignments VALUES(0, "C0006", "Week 3");
-INSERT INTO assignments VALUES(0, "C0008", "Week 3");
-INSERT INTO assignments VALUES(0, "C0005", "Week 3");
-INSERT INTO assignments VALUES(0, "C0007", "Week 3");
-
-
