756b42e2cd9bfb96c98beedbedbb032eab8ec8c9
kevin <kevin.mahasiswa@gmail.com>
2024-05-10 22:55:48 +0700

    Inisialisasi Awal View, Update web.php dan controller simple return view

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -6,27 +6,44 @@ use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
-    public function isLoggedIn(Request $req) {
+    public function isLoggedIn(Request $req)
+    {
         // Cek Apakah logged in
 
 
         return false;
     }
 
-    public function index(Request $req) {
-        if($this->isLoggedIn($req)) {
+    public function index(Request $req)
+    {
+        if ($this->isLoggedIn($req)) {
             return redirect("langify/dashboard");
-        }
-        else {
+        } else {
             return redirect("langify/login");
         }
     }
-    public function login(Request $req) {
+    public function login(Request $req)
+    {
         return view('layout.login');
     }
 
-    public function dashboard(Request $req) {
-
+    public function dashboard(Request $req)
+    {
+        return view('layout.dashboard');
     }
 
+    public function kelas(Request $req)
+    {
+        return view('layout.kelas');
+    }
+
+    public function detail_kelas(Request $req)
+    {
+        return view('layout.detail_kelas');
+    }
+
+    public function tugas(Request $req)
+    {
+        return view('layout.tugas');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [SiteController::class, "index"]);
 
 
-Route::prefix("login")->group(function() {
+Route::prefix("login")->group(function () {
     Route::get('/', [SiteController::class, "login"]);
     Route::post('/', [SiteController::class, "login"]);
-
 });
 
-Route::prefix("student")->group(function() {
+Route::prefix("student")->group(function () {
+    Route::get('/', [SiteController::class, "dashboard"]);
+    Route::get('/kelas', [SiteController::class, "kelas"]);
+    Route::get('/tugas', [SiteController::class, "tugas"]);
+    Route::get('/detail_kelas', [SiteController::class, "detail_kelas"]);
+});
+
+Route::prefix("teacher")->group(function () {
     Route::get('/', [SiteController::class, ""]);
 });
 
-Route::prefix("teacher")->group(function() {
-    Route::get('/', [SiteController::class, ""]);
-});
-
-Route::prefix("admin")->group(function() {
+Route::prefix("admin")->group(function () {
     // Route::get();
 });
