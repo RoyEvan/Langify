880e501abbce32e15b5714d24f2c6d65065d4c74
RoyEvan <77206268+RoyEvan@users.noreply.github.com>
2024-05-10 22:09:40 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,24 @@
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("langify")->group(function () {
-    Route::get('/', [SiteController::class, "index"]);
+
+Route::get('/', [SiteController::class, "index"]);
 
 
-    Route::prefix("login")->group(function() {
-        Route::get('/', [SiteController::class, "login"]);
-    });
+Route::prefix("login")->group(function() {
+    Route::get('/', [SiteController::class, "login"]);
+    Route::post('/', [SiteController::class, "login"]);
 
-    Route::prefix("student")->group(function() {
-        Route::get('/', [SiteController::class, ""]);
-    });
+});
 
-    Route::prefix("teacher")->group(function() {
-        Route::get('/', [SiteController::class, ""]);
-    });
+Route::prefix("student")->group(function() {
+    Route::get('/', [SiteController::class, ""]);
+});
 
-    Route::prefix("admin")->group(function() {
-        // Route::get();
-    });
+Route::prefix("teacher")->group(function() {
+    Route::get('/', [SiteController::class, ""]);
+});
 
+Route::prefix("admin")->group(function() {
+    // Route::get();
 });
